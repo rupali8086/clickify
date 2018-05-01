eba49d5f264ee56c2218c791369a3ace2ee6e191
rupali.meshram <meshram.rupali86@gmail.com>
2018-05-01 18:34:04 -0400

    clickify game

---
+++ clikify/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+//sets up the reusable Jumbotron component
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+	<header className = "header">
+		
+		<h3>Click on an image to earn points, but don't click on any more than once!.</h3>
+	</header>
+);
+
+export default Jumbotron;
